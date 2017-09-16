cc1e95028e39f47ce4d4d2cf2095b62f6219ce4d
KeyBoardGaming <alleneboyce@me.com>
2017-09-16 15:04:19 -0700

    Add files via upload
    Just another backup

--- notlast.java
+++ notlast.java
@@ -22,44 +22,67 @@ public class notlast {
 			int addMilk = Integer.parseInt(st.nextToken());
 			milkValues[positionOf(cowName, cowNames)]+=addMilk;
 		}
-		/*
-		for(int c = 0; c < 7; c++) {
-			System.out.println(cowNames[c] + ": " + milkValues[c]);
-		}
-		*/
+		f.close();
 		int[] originalMilkValues = new int[7];
 		for(int c = 0; c < milkValues.length; c++) {
 			originalMilkValues[c] = milkValues[c];
 		}
-		Arrays.sort(milkValues);		
+		Arrays.sort(milkValues);	
 		
-		boolean[] isNumberConnected = new boolean[7];
-		boolean[] isCowConnected = new boolean[7];
-		String[] rearrangedCowNames = new String[7];
-		for(int c= 0; c < 7; c++) {
-			int numToCheck = originalMilkValues[c];
-			String cowToCheck = cowNames[c];
-			for(int i = 0; i < 7; i++) {
-				if((milkValues[i] == numToCheck) && (!isNumberConnected[i])) {
-					isNumberConnected[i] = true;
-					rearrangedCowNames[i] = cowToCheck;
-				}
+		
+		//find the smallest value
+		int smallest = 101;
+		for(int c = 0; c < 7; c++) {
+			if(milkValues[c] < smallest) {
+				smallest = milkValues[c];
 			}
 		}
+		//System.out.println(smallest);
+		//Find the second smallest value
+		int secondSmallest = 101;
 		for(int c = 0; c < 7; c++) {
-			System.out.println(rearrangedCowNames[c] + ": " + milkValues[c]);
+			if((milkValues[c] > smallest) && (milkValues[c] < secondSmallest)) {
+				secondSmallest = milkValues[c];
+			}
 		}
+		//System.out.println(secondSmallest);
 		
-		
+		if(asInArr(secondSmallest, milkValues) > 1) {
+			out.println("TIE");
+		} else {
+			out.println(cowNames[positionOf(secondSmallest, milkValues)]);
+		}
 		out.close();
 	}
 	
-
+		
+	public static int asInArr(int a, int[] arr) {
+		int returnval = 0;
+		for(int c = 0; c < arr.length; c++) {
+			if(arr[c] == a) {
+				returnval++;
+			}
+		}
+		return returnval;
+	}
+	
 	public static int positionOf(String str, String[] cowNames) {
 		int returnval = 0;
 		
 		for(int c = 0; c < cowNames.length; c++) {
 			if(cowNames[c].equals(str)) {
+				returnval = c;
+				break;
+			}
+		}
+		
+		return returnval;
+	}
+	public static int positionOf(int str, int[] cowNames) {
+		int returnval = 0;
+		
+		for(int c = 0; c < cowNames.length; c++) {
+			if(cowNames[c] == str) {
 				returnval = c;
 				break;
 			}
