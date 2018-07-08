c9103713d83c9562400a49958c9b218a6b8e44e0
KeyBoardGaming <alleneboyce@me.com>
2018-07-07 17:05:47 -0700

    Add files via upload

--- lazy.java
+++ lazy.java
@@ -1,7 +1,8 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Arrays;
+import java.io.PrintWriter;
 import java.util.StringTokenizer;
 
 public class lazy {
@@ -13,50 +14,92 @@ public class lazy {
 		int cowLimit = Integer.parseInt(st.nextToken());
 		int[] distances = new int[grasses];
 		int[] grassValues = new int[grasses];
+		//Farthest distance for the length of the field
+		int fieldLength = 0;
 		
 		//Get all of the values from the input and assign them to an array
 		for(int c = 0; c < grasses; c++) {
 			String newLine = f.readLine();
 			st = new StringTokenizer(newLine);
 			grassValues[c] = Integer.parseInt(st.nextToken());
-			distances[c] = Integer.parseInt(st.nextToken());
+			int nextPos = Integer.parseInt(st.nextToken());
+			if(nextPos > fieldLength) fieldLength = nextPos;
+			distances[c] = nextPos;
 		}
 		
-		//Farthest distance for the length of the field
-		int fieldLength = 0;
-		for(int c = 0; c < grasses; c++) {
-			if(distances[c] >fieldLength) fieldLength = distances[c];
-		}
 		
-		int[] grass = new int[fieldLength];
-		System.out.println(grassValues.length);
-		System.out.println(fieldLength);
-		System.out.println(distances.length);
+		
+		
+		int[] grass = new int[fieldLength+1];
+		//System.out.println(distances.length);
+		//System.out.println(grassValues.length);
+		//System.out.println(distances[251]);
+		//System.out.println(fieldLength);
+	
+		//System.out.println(grass[(distances[251])-1]);
+		
 		for(int c = 0; c < distances.length; c++) {
-			System.out.println(c);
-			grass[distances[c]-1] = grassValues[c];
+			//System.out.println(distances[c]);
+			grass[(distances[c])] = grassValues[c];
 		} //Got grasses array correct
 		
-		
+		//printArr(grass);
+		//System.out.println("done assignning values");
 		int startCheck = 0;
 		int endCheck = cowLimit*2;
+		if(endCheck > grass.length) endCheck = grass.length-1;
 		int mostGrass = 0;
+		int bodyVal = 0;
+		bodyVal+=grass[startCheck];
+		//System.out.println(endCheck);
+		bodyVal+=grass[endCheck];
+		for(int c = startCheck+1; c < endCheck-1; c++) {
+			bodyVal+=grass[c];
+		}
 		while(endCheck <= fieldLength) {
+			
+			
+			
+			
+			
+				/*
 			int currSum = 0;
-			for(int c = startCheck; c < endCheck; c++) {
+			//System.out.println("From " + startCheck + " to " + endCheck + ": " + currSum);
+			for(int c = startCheck; c <= endCheck; c++) {
 				currSum+=grass[c];
+				//System.out.println("Adding " + grass[c]);
+				
 			}
-			if(currSum > mostGrass) mostGrass = currSum;
+			
+			if(currSum > mostGrass) {
+				mostGrass = currSum;
+				
+			}
+			*/
+			//Check for if this is the most grass
+			if(bodyVal >mostGrass) {
+				mostGrass = bodyVal;
+			}
+			
+			//Shifting the range
+			bodyVal-=grass[startCheck];	
+			if(endCheck < fieldLength) bodyVal+=grass[endCheck+1];
+			
+			
 			startCheck++;
 			endCheck++;
+			
 		}
 		
-		System.out.println(mostGrass);
+		//System.out.println(mostGrass);
 		
-		
+		PrintWriter out = new PrintWriter(new FileWriter("lazy.out"));
+		out.println(mostGrass);
+		out.close();
 		
 		//printArr(grass);
 	}
+	
 	public static void printArr(int[] arr) {
 		for(int c = 0; c < arr.length; c++) {
 			System.out.print(arr[c] + " ");
